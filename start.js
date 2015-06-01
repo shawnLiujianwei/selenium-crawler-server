d9c3f830e2396add30c9cd0fce32bc68f60a0a78
Shawn Liu <shawn.liu@dotter.me>
2015-06-01 21:06:34 +0800

    integrate selenium-hub-launcher

--- start.js
+++ start.js
@@ -7,31 +7,59 @@ var config = require("config");
 var Process = require("child_process");
 var Promise = require("bluebird");
 var os = require("os");
-//var seleniumServer = require("../selenium");
-//var phantomInstance = require("../phantom");
-//var listenerConfig = require("config").listener;
 var path = require("path");
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.NODE_CONFIG_DIR = path.join(__dirname, "./config");
-//var driverInstance = require("./app-driverInstance/startup");
-_setupCrawlerServer()
-    .then(function () {
-        //return _startServer();
-    })
-    .catch(function (err) {
-        logger.error(err);
-        process.exit();
-    })
+var seleniumServer = require("selenium-hub-launcher");
 
+
+function _killExisting() {
+    var names = [
+        "seleniumHub",
+        "webCrawler"
+    ];
+    return Promise.map(names, function (name) {
+        var command = [
+            "pm2",
+            "delete",
+            name
+        ];
+        return _executeCommand(command, true);//always return resolve(), just in case the process doesn't exist
+    })
+}
 
 function _setupCrawlerServer() {
+    var command = [];
+    command.push("pm2 start");
+    command.push(seleniumServer.startupFile);
+    command.push("--name=seleniumHub");
+    if (process.env.NODE_ENV === "production") {
+        command.push("--max-memory-restart");
+        command.push(_getMaxMemory());
+    }
+    return _executeCommand(command);
+}
+
+function _startServer() {
+    var appConfig = config.app;
+    var command = [];
+    command.push("pm2 start");
+    command.push(path.join(__dirname, "./app.js"));
+    command.push("--name=webCrawler");
+    if (process.env.NODE_ENV === "production") {
+        //command.push("i " + os.cpus().length);
+        command.push("i 2");
+        command.push("--max-memory-restart");
+        command.push(_getMaxMemory());
+    }
+    return _executeCommand(command);
+
+}
+
+function _executeCommand(command, alwaysTrue) {
     return new Promise(function (resolve, reject) {
-        var command = [];
-        command.push("pm2 start");
-        command.push("app-driverInstance/server.js");
-        command.push("--name=phantomCluster");
         var cp = Process.exec(command.join(" "), function (err, data) {
-            if (err) {
+            if (err && !alwaysTrue) {
                 reject(err);
             } else {
                 resolve();
@@ -46,50 +74,21 @@ function _setupCrawlerServer() {
     })
 }
 
-function _startServer() {
-    var appConfig = config.app;
-    var command = [];
-    command.push("pm2 start");
-    command.push(appConfig.script);
-    command.push("--name=" + appConfig.name);
-    if (appConfig.mode === "cluster") {
-        command.push("i " + appConfig.instances || os.cpus().length);
-        command.push("--max-memory-restart");
-        command.push(_getMaxMemory());
-        return new Promise(function (resolve, reject) {
-            Process.exec(command.join(" "), function (err, data) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
-        })
-    } else {
-        //command.push("pm2 start");
-        //command.push(appConfig.script);
-        //command.push("--name=webdriver")
-        return new Promise(function (resolve, reject) {
-            var cp = Process.exec(command.join(" "), function (err, data) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
-            cp.stdout.on('data', function (data) {
-                logger.info(data.toString());
-            });
-            cp.stderr.on('data', function (data) {
-                logger.error(data.toString());
-            });
-        })
-    }
-
-}
 
 function _getMaxMemory() {
     var freeM = os.totalmem();
     //var totalM = os.freemem();
     return Math.round(freeM / (1024 * 1024) * 0.9);
 }
+
+_killExisting()
+    .then(function () {
+        return _setupCrawlerServer();
+    })
+    .then(function () {
+        return _startServer();
+    })
+    .catch(function (err) {
+        logger.error(err);
+        process.exit();
+    })
